2bf39284ce3df4365121c48c93b8caf8c6142050
nguyenndt278 <53121073+nguyenndt278@users.noreply.github.com>
2020-01-02 19:21:38 -0600

    push 3

--- static/js/viral_search.js
+++ static/js/viral_search.js
@@ -37,17 +37,24 @@ function search_most_view() {
 function onSearchResponse(response) {
   // var responseString = JSON.stringify(response, '', 2);
   // document.getElementById('response').innerHTML = responseString;
+
   video_id_list = [];
-  tags = []
+  tags = [];
+  titles=[];
+  views_count=[];
+  like_count=[];
   for (var i = 0; i < response.items.length; i++) {
 
     var views = response.items[i].statistics.viewCount;
+    views_count.push(views);
     var comments = response.items[i].statistics.commentCount;
     var likes = response.items[i].statistics.likeCount;
+    like_count.push(likes);
     var dislikes = response.items[i].statistics.dislikeCount;
     var publishedAt = response.items[i].snippet.publishedAt;
     var channelId = response.items[i].snippet.channelId;
     var title = response.items[i].snippet.title;
+    titles.push(title);
     var description = response.items[i].snippet.description;
     var channelTitle = response.items[i].snippet.channelTitle;
     var tag = response.items[i].snippet.tags;
@@ -59,8 +66,112 @@ function onSearchResponse(response) {
     buildTable(views, comments, likes, dislikes, publishedAt, title, description, channelTitle, channelId, videoID, video);
     // buildTable_tag(tag);
   }
-  // console.log(tags);
+
   var obj1 = tags.toString();
+
+  var obj_circular = {};
+  titles.forEach((key, i) => obj_circular[key] = views_count[i]);
+  // console.log(obj_circular);
+  var data = Object.entries(obj_circular).map((k, v) => { return { "title_name": (k.slice(0, 1)).toString(), "view_name":(k.slice(1, 2)).toString() } });
+  // var data=[data_1];
+  console.log(data);
+  // console.log(JSON.stringify(data));
+  // console.log(Object.keys(data)[0]);
+
+  var margin = {top: 100, right: 0, bottom: 0, left: 0},
+  width = 760 - margin.left - margin.right,
+  height = 760 - margin.top - margin.bottom,
+  innerRadius = 90,
+  outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
+
+// append the svg object
+var svg = d3.select("#my_dataviz_circular")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
+
+// d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum.csv", function(data) {
+
+// Scales
+// var data=my_circular;
+var x = d3.scaleBand()
+    .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+    .align(0)                  // This does nothing
+    .domain(data.map(function(d) { return d.title_name; })); // The domain of the X axis is the list of states.
+var y = d3.scaleRadial()
+    .range([innerRadius, outerRadius])   // Domain will be define later.
+    .domain([0, 25000000]); // Domain of Y is from 0 to the max seen in the data
+
+
+    // create a tooltip
+  var tooltip = d3.select("#my_dataviz_circular")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "2px")
+  .style("border-radius", "2px")
+  // .style("padding", "1px")
+
+// Three function that change the tooltip when user hover / move / leave a cell
+var mouseover = function(d) {
+  tooltip
+    .style("opacity", 1)
+  d3.select(this)
+    .style("stroke", "black")
+    .style("opacity", 1)
+}
+var mousemove = function(d) {
+  tooltip
+    .html("Title: " + d.title_name)
+    .style("left", (d3.mouse(this)[0]+1) + "px")
+    .style("top", (d3.mouse(this)[1]) + "px")
+}
+var mouseleave = function(d) {
+  tooltip
+    .style("opacity", 0)
+  d3.select(this)
+    .style("stroke", "none")
+    .style("opacity", 0.8)
+}
+
+// Add the bars
+svg.append("g")
+  .selectAll("path")
+  .data(data)
+  .enter()
+  .append("path")
+    .attr("fill", "#69b3a2")
+    .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+        .innerRadius(innerRadius)
+        .outerRadius(function(d) { return y(d.view_name); })
+        .startAngle(function(d) { return x(d.title_name); })
+        .endAngle(function(d) { return x(d.title_name) + x.bandwidth(); })
+        .padAngle(0.01)
+        .padRadius(innerRadius))
+  .on("mouseover", mouseover)
+  .on("mousemove", mousemove)
+  .on("mouseleave", mouseleave)
+// console.log(data.title_name);
+
+// Add the labels
+svg.append("g")
+    .selectAll("g")
+    .data(data)
+    .enter()
+    .append("g")
+      .attr("text-anchor", function(d) { return (x(d.view_name) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+      .attr("transform", function(d) { return "rotate(" + ((x(d.title_name) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d.view_name)+10) + ",0)"; })
+    .append("text")
+      .text(function(d){return(d.view_name)})
+      .attr("transform", function(d) { return (x(d.title_name) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+      .style("font-size", "11px")
+      .attr("alignment-baseline", "middle");
+
+// });
 
   function wordCount(myString) {
     var stringArray = myString.split(",");
